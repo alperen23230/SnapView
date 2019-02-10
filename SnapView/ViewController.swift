d04ae23f85e662019d1f89ce2f1c70dbd8d7dc39
Alperen Ünal <alperenunal@alperens-macbook-air.local>
2019-02-10 16:37:32 +0300

    Initial Commit

---
+++ SnapView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapView
+//
+//  Created by Alperen Ünal on 10.02.2019.
+//  Copyright © 2019 Alperen Ünal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
